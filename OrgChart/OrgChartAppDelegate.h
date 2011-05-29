e8681fe6c3824463c7bd2361d8a01084802748ff
beepscore <support@beepscore.com>
2011-05-28 23:59:47 -0700

    Add Core Data properties and method applicationDocumentsDirectory. Reference Pro Core Data Ch 2 p 47-48.

--- OrgChart/OrgChartAppDelegate.h
+++ OrgChart/OrgChartAppDelegate.h
@@ -7,15 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CoreData/CoreData.h"
 
 @class OrgChartViewController;
 
 @interface OrgChartAppDelegate : NSObject <UIApplicationDelegate> {
-
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-
 @property (nonatomic, retain) IBOutlet OrgChartViewController *viewController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+//Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
